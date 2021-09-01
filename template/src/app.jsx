77d875fb59a7db334b55e05a994c94e6210244b9
hong li <lihongitchina@gmail.com>
2021-09-01 10:51:19 +0800

    update style

--- template/src/app.jsx
+++ template/src/app.jsx
@@ -10,7 +10,8 @@ function App(){
 	return <div className="page" >
     <div className="content" >
       <img src={image} />
-      <p className="title" >⭐️  Fvx, an automatic, convenience and flexible scaffold Tooling for React Development. ⭐️</p>
+			<h1>Fvx</h1>
+      <p className="title" >⭐️  An automatic, convenience and flexible scaffold Tooling for React Development. ⭐️</p>
     </div>
   </div>
 
